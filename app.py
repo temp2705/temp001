2826155712745dd76b34277ba47b58369f80fdc6
temp2705 <deeruvin@gmail.com>
2024-07-14 23:11:39 +0530

    hide

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 import streamlit as st
 
-hide_menu_style = """
-        <style>
-        #MainMenu {visibility: hidden;}
-        </style>
-        """
-st.markdown(hide_menu_style, unsafe_allow_html=True)
+#MainMenu {
+  visibility: hidden;
+}
 
 
 import yfinance as yf
